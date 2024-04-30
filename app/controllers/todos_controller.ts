48657d383a326bba6b42213b0db63f05df47f2c6
nadirgharbi <116977046+nadirgharbi@users.noreply.github.com>
2024-04-30 17:47:27 +0200

    todos endpoint

--- app/controllers/todos_controller.ts
+++ app/controllers/todos_controller.ts
@@ -1,5 +1,5 @@
 import Todo from '#models/todo'
-import { createPostValidator, updatePostValidator } from '#validators/todo'
+import { updatePostValidator } from '#validators/todo'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class TodosController {
@@ -34,15 +34,15 @@ export default class TodosController {
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  // async show({ params }: HttpContext) {}
 
   /**
    * Edit individual record
    */
-  async edit({ params }: HttpContext) {}
+  // async edit({ params }: HttpContext) {}
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  // async destroy({ params }: HttpContext) {}
 }

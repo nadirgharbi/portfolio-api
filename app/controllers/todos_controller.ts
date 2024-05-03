95667eb9cf4617aa62323167431b323c03bd7bf0
nadirgharbi <116977046+nadirgharbi@users.noreply.github.com>
2024-05-03 12:07:29 +0200

    ready to deploy

--- app/controllers/todos_controller.ts
+++ app/controllers/todos_controller.ts
@@ -10,22 +10,22 @@ export default class TodosController {
     // return Todo.all()
     try {
       // Vérifier si l'utilisateur est correctement authentifié
-      await auth.authenticate();
+      await auth.authenticate()
 
       // Récupérer l'utilisateur authentifié
-      const user = auth.user!;
-      
+      const user = auth.user!
+
       // Récupérer l'ID de l'utilisateur
-      const userID = user.id;
-      
+      const userID = user.id
+
       // Récupérer les tâches de l'utilisateur
-      const userTodos = await Todo.query().where('userID', userID).exec();
-      
-      return userTodos;
+      const userTodos = await Todo.query().where('userID', userID).exec()
+
+      return userTodos
     } catch (error) {
-      console.log(error);
+      console.log(error)
       // Gérer les erreurs ici
-      return {}; // Retourner une réponse vide en cas d'erreur 
+      return {} // Retourner une réponse vide en cas d'erreur
     }
   }
 
@@ -38,18 +38,6 @@ export default class TodosController {
    * Handle form submission for the create action
    */
   async store({ request, response }: HttpContext) {
-    // try {
-    //   const userId = auth.user!.id // Obtenez l'ID de l'utilisateur connecté
-    //   // const payload = { ...request.only(['title', 'completed']), userID: userId }
-
-    //   // const todo = await Todo.create(payload)
-    //   // return response.created(todo)
-    //   // console.log(payload);
-
-    //   return response.ok("ok")
-    // } catch (error) {
-    //   return response.badRequest(error.messages)
-    // }
     try {
       const payload = await request.validateUsing(createTodoValidator)
       const todo = await Todo.create(payload)
@@ -64,29 +52,7 @@ export default class TodosController {
   /**
    * Handle form submission for the edit action
    */
-  async update({ auth, params, request, response }: HttpContext) {
-    // try {
-    //   const userId = auth.user!.id // Obtenez l'ID de l'utilisateur connecté
-    //   const todoId = params.id
-
-    //   const todo = await Todo.findOrFail(todoId)
-
-    //   if (todo.userID !== userId) {
-    //     return response.forbidden({
-    //       message: "Vous n'êtes pas autorisé à mettre à jour cette tâche.",
-    //     })
-    //   }
-
-    //   const payload = request.only(['title', 'completed'])
-
-    //   todo.merge(payload)
-    //   await todo.save()
-
-    //   return response.ok(todo)
-    // } catch (error) {
-    //   // Gérer les erreurs ici
-    // }
-
+  async update({ params, request, response }: HttpContext) {
     try {
       const todoId = params.id
 
@@ -106,22 +72,6 @@ export default class TodosController {
    * Handle form submission for the delete action
    */
   async destroy({ params, response }: HttpContext) {
-    // try {
-    //   const userId = auth.user!.id // Obtenez l'ID de l'utilisateur connecté
-    //   const todoId = params.id
-
-    //   const todo = await Todo.findOrFail(todoId)
-
-    //   if (todo.userID !== userId) {
-    //     return response.forbidden({ message: "Vous n'êtes pas autorisé à supprimer cette tâche." })
-    //   }
-
-    //   await todo.delete()
-
-    //   return response.noContent()
-    // } catch (error) {
-    //   // Gérer les erreurs ici
-    // }
     try {
       const todoId = params.id
 
--- app/models/user.ts
+++ app/models/user.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column, hasMany, hasManyThrough, hasOne } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 import type { HasMany } from '@adonisjs/lucid/types/relations'
